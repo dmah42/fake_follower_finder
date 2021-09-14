450178d8866a12121e7fa1274f957942112497b1
Dominic Hamon <dma+github@stripysock.com>
2021-09-14 17:38:38 +0100

    refactor into methods

--- fake_follower_finder.py
+++ fake_follower_finder.py
@@ -13,76 +13,87 @@ parser.add_argument('user', type=str, help='the user name to check')
 
 args = parser.parse_args()
 
-consumer_key = args.key
-consumer_secret = args.secret
+def authenticate():
+    consumer_key = args.key
+    consumer_secret = args.secret
 
-screen_name = args.user
-params = {"user.fields": "created_at,public_metrics,verified"}
+    # Get request token
+    REQUEST_TOKEN_URL = "https://api.twitter.com/oauth/request_token"
+    oauth = OAuth1Session(consumer_key, client_secret=consumer_secret, callback_uri='oob')
 
-# Get request token
-REQUEST_TOKEN_URL = "https://api.twitter.com/oauth/request_token"
-oauth = OAuth1Session(consumer_key, client_secret=consumer_secret, callback_uri='oob')
+    try:
+        fetch_response = oauth.fetch_request_token(REQUEST_TOKEN_URL)
+    except ValueError:
+        sys.exit("There may have been an issue with the consumer_key or consumer_secret you entered.")
 
-try:
-    fetch_response = oauth.fetch_request_token(REQUEST_TOKEN_URL)
-except ValueError:
-    sys.exit("There may have been an issue with the consumer_key or consumer_secret you entered.")
+    resource_owner_key = fetch_response.get("oauth_token")
+    resource_owner_secret = fetch_response.get("oauth_token_secret")
+    print("Got OAuth token: %s" % resource_owner_key)
 
-resource_owner_key = fetch_response.get("oauth_token")
-resource_owner_secret = fetch_response.get("oauth_token_secret")
-print("Got OAuth token: %s" % resource_owner_key)
+    # # Get authorization
+    BASE_AUTHORIZATION_URL = "https://api.twitter.com/oauth/authorize"
+    authorization_url = oauth.authorization_url(BASE_AUTHORIZATION_URL)
+    print("Please go here and authorize: %s" % authorization_url)
+    verifier = input("Paste the PIN here: ")
 
-# # Get authorization
-BASE_AUTHORIZATION_URL = "https://api.twitter.com/oauth/authorize"
-authorization_url = oauth.authorization_url(BASE_AUTHORIZATION_URL)
-print("Please go here and authorize: %s" % authorization_url)
-verifier = input("Paste the PIN here: ")
+    # Get the access token
+    ACCESS_TOKEN_URL = "https://api.twitter.com/oauth/access_token"
+    oauth = OAuth1Session(
+        consumer_key,
+        client_secret=consumer_secret,
+        resource_owner_key=resource_owner_key,
+        resource_owner_secret=resource_owner_secret,
+        verifier=verifier,
+    )
+    oauth_tokens = oauth.fetch_access_token(ACCESS_TOKEN_URL)
 
-# Get the access token
-ACCESS_TOKEN_URL = "https://api.twitter.com/oauth/access_token"
-oauth = OAuth1Session(
-    consumer_key,
-    client_secret=consumer_secret,
-    resource_owner_key=resource_owner_key,
-    resource_owner_secret=resource_owner_secret,
-    verifier=verifier,
-)
-oauth_tokens = oauth.fetch_access_token(ACCESS_TOKEN_URL)
+    access_token = oauth_tokens["oauth_token"]
+    access_token_secret = oauth_tokens["oauth_token_secret"]
 
-access_token = oauth_tokens["oauth_token"]
-access_token_secret = oauth_tokens["oauth_token_secret"]
+    return OAuth1Session(
+        consumer_key,
+        client_secret=consumer_secret,
+        resource_owner_key=access_token,
+        resource_owner_secret=access_token_secret,
+    )
 
-# Make the request
-oauth = OAuth1Session(
-    consumer_key,
-    client_secret=consumer_secret,
-    resource_owner_key=access_token,
-    resource_owner_secret=access_token_secret,
-)
 
-user_response = oauth.get("https://api.twitter.com/2/users/by/username/{}".format(screen_name))
+def get_user_id(oauth):
+    screen_name = args.user
+    user_response = oauth.get("https://api.twitter.com/2/users/by/username/{}".format(screen_name))
 
-if user_response.status_code != 200:
-    raise Exception(
-        "Request returned an error: {} {}".format(user_response.status_code, user_response.text))
+    if user_response.status_code != 200:
+        raise Exception(
+            "Request returned an error: {} {}".format(user_response.status_code, user_response.text))
 
-print("Response code: {}".format(user_response.status_code))
+    print("Response code: {}".format(user_response.status_code))
 
-json_response = user_response.json()
+    json_response = user_response.json()
 
-print(json.dumps(json_response, indent=2, sort_keys=True))
+    print(json.dumps(json_response, indent=2, sort_keys=True))
 
-user_id = json_response['id']
+    return json_response['id']
 
-followers_response = oauth.get(
-    "https://api.twitter.com/2/users/{}/followers".format(user_id), params=params)
 
-if followers_response.status_code != 200:
-    raise Exception("Request returned an error: {} {}".format(
-      followers_response.status_code, followers_response.text))
+def get_followers(oauth, user_id):
+    params = {"user.fields": "created_at,public_metrics,verified"}
+    followers_response = oauth.get(
+        "https://api.twitter.com/2/users/{}/followers".format(user_id), params=params)
 
-print("Response code: {}".format(followers_response.status_code))
+    if followers_response.status_code != 200:
+        raise Exception("Request returned an error: {} {}".format(
+        followers_response.status_code, followers_response.text))
 
-json_response = followers_response.json()
+    print("Response code: {}".format(followers_response.status_code))
 
-print(json.dumps(json_response, indent=2, sort_keys=True))
+    json_response = followers_response.json()
+
+    print(json.dumps(json_response, indent=2, sort_keys=True))
+
+    return json_response['data']
+
+
+oauth = authenticate()
+user_id = get_user_id(oauth)
+followers = get_followers(oauth, user_id)
+print(followers)
